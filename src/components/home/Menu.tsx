a979ef68b736dcc9538fdf55a03e005cd629d1c3
thetoolshedrep <fixed.my.ads@gmail.com>
2025-05-29 16:37:43 -0400

    adjust menu

--- src/components/home/Menu.tsx
+++ src/components/home/Menu.tsx
@@ -6,9 +6,6 @@ import { UserButton } from "@clerk/nextjs";
 export default function Menu() {
   return (
     <header className="w-full px-6 py-4 flex justify-between items-center bg-gray-950 text-white border-b border-gray-800">
-      {/* Logo */}
-      <div className="text-xl font-bold">FMA</div>
-
       {/* Navigation */}
       <nav className="space-x-4 flex items-center">
         <Link href="/upload" className="hover:underline">
